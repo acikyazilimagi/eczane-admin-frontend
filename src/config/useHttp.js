087d4186b7a0ff753257e60dd0b2673a892c673f
Engin Karataş <enginkaratas99@gmail.com>
2023-02-11 21:24:28 +0300

    Api endpoind updated

--- src/config/useHttp.js
+++ src/config/useHttp.js
@@ -1,15 +1,16 @@
 export const useHTTPOptions = {
-  responseType: 'json',
-  url: 'https://apieczane.afetharita.com/api',
+  responseType: "json",
+  url: "https://eczaneapi.afetharita.com/api",
   // url: 'http://localhost:8080/api',
   interceptors: {
     // every time we make an http request, this will run 1st before the request is made
     // url, path and route are supplied to the interceptor
     // request options can be modified and must be returned
     request: async ({ options, url, path, route }) => {
-      const token = localStorage.getItem('token')
-      if (token && token?.length > 0) options.headers.Authorization = `${token}`
-      return options
+      const token = localStorage.getItem("token");
+      if (token && token?.length > 0)
+        options.headers.Authorization = `${token}`;
+      return options;
     },
-  }
-}
+  },
+};
