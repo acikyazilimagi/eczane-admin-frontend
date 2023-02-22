96a52011e3d8953f599b093d4c92f251a94e7b6a
Burak Kucukparmaksiz <burakkp@gmail.com>
2023-02-22 23:49:20 +0100

    Latitude Longitude ile sehir kontrolu eklendi

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-export const getDateQuery = () => "?" + Date.now()
+export const getDateQuery = () => "?" + Date.now();
+
+export const verifyLatLong = (refLat, refLong, lat, long, km) => {
+  let ky = 40000 / 360;
+  let kx = Math.cos((Math.PI * refLat) / 180.0) * ky;
+  let dx = Math.abs(refLong - long) * kx;
+  let dy = Math.abs(refLat - lat) * ky;
+  let result = Math.sqrt(dx * dx + dy * dy) <= km;
+
+  if (result) {
+    return true;
+  } else {
+    alert("Lütfen belirtilen şehir sınırları içerisinde bir lokasyon giriniz.");
+
+    return false;
+  }
+};
